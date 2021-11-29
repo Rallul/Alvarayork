82387412ad3e890e2e60b4f54af5726599a9b11e
anuar <anuar@gmail.com>
2021-11-29 14:35:35 -0600

    Ten

--- index.php
+++ index.php
@@ -7,69 +7,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Mialvarado</title>
+    <!--Este codigo es para que los iconos funcionen-->
+    <script src="https://kit.fontawesome.com/ace0a1e6ef.js" crossorigin="anonymous"></script>
+    <!-- este es para que se ponga una imagen en la pestaña como tipo logo-->
+    <link rel="shortcut icon" href="img/yayarislogo.png" type="image/x-icon">
+    <?php  require_once "EncabezadoAdmin.php"; ?>
 
-    <!-- Extenciones -->
-    <?php include('principal/extenciones.php'); ?>
+
 
     <!--Este es para crear la ruta para los estilos-->
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/margenes.css">
 
 </head>
 
-<!-- nav -->
-<?php include('principal/nav.php'); ?>
 
 <body>
     <!--Aqui metemos el encabezado-->
     <header>
-
-     <!-- La nav toda wapa-->
- <nav id="menu" class="navbar navbar-expand-lg navbar-light">
-     <a class="navbar-brand" href="index.php" style="color: #ffff;"><b>Mi Alvarado</b></a>
-
-     <!-- No se para que sirve este cacho de codigo -->
-     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
-     </button>
-     <!-------------------------------------------------------------------------------------------------------->
-
-     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul class="navbar-nav mr-auto">
-             <li class="nav-item active">
-                 <a class="nav-link" href="index.php" style="color: #ffff;">Inicio<span class="sr-only">(current)</span></a>
-             </li>
-             <li class="nav-item">
-                 <a class="nav-link" href="#" style="color: #ffff;">Contactos</a>
-             </li>
-             <!-- Este es la que despeja la cajita de opciones -->
-             <li class="nav-item dropdown">
-                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #ffff;">AlvaraKejas</a>
-                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-                     <form class="form-horizontal" name="formUser"><a class="dropdown-item" href=" " data-toggle="modal" data-target="#myModal">Iniciar Sesion</a></form>
-                     <!-- ------------------------------------------------------------------------------------ -->
-                     <form class="form-horizontal" name="formUser"><a class="dropdown-item" data-toggle="modal" data-target="#myModal2">Registro</a></form>
-                     <!-- ------------------------------------------------------------------------------------ -->
-                     <div class="dropdown-divider"></div>
-                     <a class="dropdown-item" href="principal/administrador.php">Administrador</a>
-                 </div>
-             </li>
-             <li class="nav-item">
-                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Puto el que lo lea</a>
-             </li>
-         </ul>
-         <form class="form-inline my-2 my-lg-0">
-             <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
-         </form>
-     </div>
- </nav>
         <!-- Parte en la que se ponen el titulo y etc -->
         <section>
-            <h3 class="slogan" style="color: #ffff; text-align:left;" aling="top"><img class="logo" src="imagenes/Mialvatado.png" height="250px" alt=""> <img class="letras" src="imagenes/AAAAA.png" alt=""></h3>
+            <h3 class="slogan" style="color: #ffff; text-align:left;" aling="top"><img class="logo" src="img/Mialvatado.png" height="250px" alt=""> <img class="letras" src="img/AAAAA.png" alt=""></h3>
         </section>
 
-        <!--Este codigo es para la creacion de una wave para el diseño-->
+        <!--Este codigo es para la creacion de una wave para el dciseño-->
         <div class="wave" style="height: 200px; overflow: hidden;">
             <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
                 <path d="M0.00,49.98 C120.48,185.03 276.80,-32.06 500.00,49.98 L507.62,160.36 L0.00,150.00 Z" style="stroke: none; fill: #ffff;"></path>
@@ -94,7 +54,7 @@
                 <div class="card-deck">
                     <a href="google.com">
                         <div class="card">
-                            <img src="imagenes/historia.jpeg" class="card-img-top" alt="...">
+                            <img src="img/historia.jpeg" class="card-img-top" alt="...">
                             <div class="capa">
                                 <h2>Historia</h2>
                             </div>
@@ -103,7 +63,7 @@
 
 
                     <div class="card">
-                        <img src="imagenes/festividades.jpeg" class="card-img-top" alt="...">
+                        <img src="img/festividades.jpeg" class="card-img-top" alt="...">
                         <div class="capa">
                             <h2>Festividades</h2>
                         </div>
@@ -111,7 +71,7 @@
 
 
                     <div class="card">
-                        <img src="imagenes/cultura.jpeg" class="card-img-top" alt="...">
+                        <img src="img/cultura.jpeg" class="card-img-top" alt="...">
                         <div class="capa">
                             <h2>Cultura</h2>
                         </div>
@@ -123,131 +83,21 @@
     </center>
     <br><br><br>
 
-    <!-- Modal Iniciar Sesion-->
-    <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog modal-lg">
 
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-
-                <!-- Clase contenedora del formulario dentro del modal -->
-                <div class="modal-body">
-                    <center>
-                        <h1 class="h2s" style="color: #ffff;">Iniciar Sesión</h1>
-                        <img class="img-container" src="imagenes/login.svg" style="height: 100px;" alt="">
-                        <br><br>
-                        <form>
-                            <div class="form-group1">
-                                <input type="email" required>
-                                <label class="lblC" for="exampleInputEmail1"><span class="stxt">Correo</span></label>
-
-                            </div>
-
-                            <div class="form-group2">
-                                <input type="password" required>
-                                <label class="lblC" for="exampleInputPassword1"><span class="stxt">Contraseña</span></label>
-                            </div>
-                            <br>
-                            <div>
-                                <button type="button" class="btn btn-outline-light">Enviar</button>
-                                <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
-                                <br><br>
-                                <p class="p-container">¿No tienes cuenta? <a href="" style="color: #ffff;" data-target="#myModal2" data-toggle="modal">Registrate</a></p>
-                            </div>
-                        </form>
-                    </center>
+    <!-- Foother -->
+    <section>
+        <footer>
+            <div class="contenedor-footer">
+                <div class="contenedor-foo">
+                    <h4>Cualquier Duda</h4>
+                    <p>MiAlvarado@gmail.com</p>
                 </div>
             </div>
-        </div>
-    </div>
-
-    <!-- Modal Registro-->
-    <div class="modal fade" id="myModal2" role="dialog">
-        <div class="modal-dialog modal-lg">
-
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-
-                <!-- Clase contenedora del formulario dentro del modal -->
-                <div class="modal-body">
-                    <center>
-                        <h1 class="h1" style="color: #ffff;">Registro</h1>
-                        <img class="img-container" src="imagenes/verify.png" style="height: 100px;" alt="">
-                        <br><br>
-                        <!-- Aqui se pone estas cosa para hacer la coneccion a la base de dartos -->
-                        <form action="registro.php" name="" method="POST">
-
-                            <div class="form-group0">
-                                <input type="text" required>
-                                <label class="lblC" for="exampleInputEmail1" name="nombre"><span class="stxt">Nombre</span></label>
-
-                            </div>
-
-                            <div class="form-group1">
-                                <input type="email" required>
-                                <label class="lblC" for="exampleInputEmail1" name="correo"><span class="stxt">Correo</span></label>
-
-                            </div>
-
-                            <div class="form-group2">
-                                <input type="password" required>
-                                <label class="lblC" for="exampleInputPassword1" name="contraseña"><span class="stxt">Contraseña</span></label>
-                            </div>
-
-                            <div class="form-group3">
-                                <input type="password" required>
-                                <label class="lblC" for="exampleInputPassword1" name="conficontra"><span class="stxt">Confirmar Contraseña</span></label>
-                            </div>
-                            <br>
-                            <div action_page="./principal/registro.php" ;>
-                                <button id="enviar" type="button" class="btn btn-outline-light">Enviar</button>
-                                <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
-                                <br><br>
-                                <p class="p-container">¿Ya tienes cuenta? <a href="" style="color: #ffff;" data-target="#myModal" data-toggle="modal">Iniciar Sesion</a></p>
-                            </div>
-                        </form>
-                    </center>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Fooder -->
-    <?php include('principal/foother.php'); ?>
+            <h2 class="titulo-final">&copy; Xenogenesis </h2>
+        </footer>
+    </section>
 
 </body>
 
-
-<!-- Librerias para boostrap js jquery -->
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Esto es la condicional para que la nav se ponga transparente y viceversa -->
-<script>
-    $(window).scroll(function() {
-        if ($("#menu").offset().top > 350) {
-            $("#menu").addClass("bcolor");
-        } else {
-            $("#menu").removeClass("bcolor");
-        }
-    });
-</script>
-
-<script>
-    $(window).scroll(function() {
-        if ($("#menu").offset().top > 350) {
-            $("#menu").addClass("navop");
-        } else {
-            $("#menu").removeClass("navop");
-        }
-    });
-</script>
 
 </html>
